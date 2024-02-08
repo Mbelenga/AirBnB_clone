851d1a0512689e42ef0dee081012462766267b52
Mbelenga <mbelengaterence@gmail.com>
2024-02-08 04:03:56 -0800

    update the datetime

--- models/__init__.py
+++ models/__init__.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
-""""
+"""
 This is an init model
 It is supposed to create a unique File Storage
-""""
+"""
 from models.engine.file_storage import FileStorage
 
 storage = FileStorage()
--- models/base_model.py
+++ models/base_model.py
@@ -1,25 +1,30 @@
 #!/usr/bin/python3
-""""
+"""
 Defines the basemodel class which provides the public instance
 attributes and methods and also the serialization/deserialization
 process.
-""""
+"""
+
+
 import uuid
 from datetime import datetime
 import models
 
+
 class BaseModel:
-""""
+
+
+"""
 A BaseModel class
-""""
+"""
 def __init__(self, *args, *kwargs):
-    """"
-    Initializing an instance of the base model class the attributes
-    to be used created_at and updated_at which are datetime attribute
-    the id which is a string attrbute.
-    The methods to be used include __str__method tha t returns a string
-    representation of the object.
-    """"
+"""
+Initializing an instance of the base model class the attributes
+to be used created_at and updated_at which are datetime attribute
+the id which is a string attrbute.
+The methods to be used include __str__method tha t returns a string
+representation of the object.
+"""
 if kwargs:
     for key, value in kwargs.items():
         if key == "created_at" or key == "updated_at":
@@ -30,3 +35,14 @@ if kwargs:
         self.created_at = datetime.now()
         self.upated_at = self.created_at
         models.storage.new(self)
+
+def __str__(self):
+"""sets the basemodel"""
+class_name = self.__class__.__name__
+return f"[{class_name}] ({self.id}) {self.__dict__}"
+
+def save(self):
+"""Updates the datetime with current datetime"""
+self.Updated_at = datetime.now()
+models.storage.save()
+
