dfad3a1d2252100cd0e64b2b3d1f3659399eb704
Mbelenga <mbelengaterence@gmail.com>
2024-02-09 20:03:25 -0800

    Defines the amenity class

---
+++ models/amenity.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""
+Inherits from the BaseModel
+It defines the amenity class
+"""
+from models.base_model import BaseModel
+
+
+class Amenity(BaseModel):
+    """
+    Public class attributes:
+    name:string-empty string
+    """
+    name = ""
