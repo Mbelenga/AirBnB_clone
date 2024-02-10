fa9c85014ac7933ee3f2069c685726bdc31e51ec
Mbelenga <mbelengaterence@gmail.com>
2024-02-09 19:47:09 -0800

    pycodestyle

--- models/city.py
+++ models/city.py
@@ -2,11 +2,11 @@
 """
 Defines the city class
 """
-    from models.base_model import BaseModel
+from models.base_model import BaseModel
 
 
     class City(BaseModel):
-        """
+    """
         Public class attributes:
     state_id:string-empty string:will be the stt
 e.id
