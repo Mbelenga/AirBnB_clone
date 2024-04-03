9b134190e947097216e85e36159ae95abcf9f97d
Mbelenga <mbelengaterence@gmail.com>
2024-04-03 07:32:43 -0700

    Updated

--- models/__init__.py
+++ models/__init__.py
@@ -1,8 +1,6 @@
 #!/usr/bin/python3
-"""
-This is an init model
-It is supposed to create a unique File Storage
-"""
+""" init models """
+
 from models.engine.file_storage import FileStorage
 
 storage = FileStorage()
--- models/base_model.py
+++ models/base_model.py
@@ -1,64 +1,44 @@
 #!/usr/bin/python3
-"""
-Defines the basemodel class which provides the public instance
-attributes and methods and also the serialization/deserialization
-process.
-"""
+"""Defines the BaseModel"""
 
 
-import uuid
-from datetime import datetime
+from uuid import uuid4
+from datetime import date, datetime
 import models
 
 
 class BaseModel:
-    """
-    A basemodel class
-    """
+    """The super class"""
+    def __init__(self, *args, **kwargs):
+        """Initializing the base model"""
+        if len(kwargs) != 0:
+            for key, value in kwargs.items():
+                if key == "__class__":
+                    continue
+                elif key == "created_at" or key == "updated_at":
+                    setattr(self, key, datetime.fromisoformat(value))
+                else:
+                    setattr(self, key, value)
+        else:
+            self.id = str(uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = self.created_at
+            models.storage.new(self)
 
+    def __str__(self):
+        """sets the print behaviour of the base model"""
+        class_name = self.__class__.__name__
+        return "[{}] ({}) {}".format(class_name, self.id, self.__dict__)
 
-def __init__(self, *args, **kwargs):
-    """
-    Initializing an instance of the base model class the attributes
-    to be used created_at and updated_at which are datetime attribute
-    the id which is a string attrbute.
-    The methods to be used include __str__method tha t returns a string
-    representation of the object.
-    """
+    def save(self):
+        """updates up_dated with current datetime"""
+        self.updated_at = datetime.now()
+        models.storage.save()
 
-
-if kwargs:
-    for key, value in kwargs.items():
-        if key == "created_at" or key == "updated_at":
-            value = datetime.strptime(value, "%Y-%m-%dT%H:%M:%S.%f")
-        setattr(self, key, value)
-    else:
-        self.id = str(uuid4())
-        self.created_at = datetime.now()
-        self.upated_at = self.created_at
-        models.storage.new(self)
-
-
-def __str__(self):
-    """sets the basemodel"""
-
-
-class_name = self.__class__.__name__
-return "[{}] ({}) {}" .format"[{class_name}] ({self.id}) {self.__dict__}"
-
-
-def save(self):
-    """Updates the datetime with current datetime"""
-
-
-self.Updated_at = datetime.now()
-models.storage.save()
-
-
-def to_dict(self):
-    """Returns a dictionary"""
-    new_dict = self.__dict__.copy()
-    new_dict["created_at"] = self.created_at.isoformat()
-    new_dict["updated_at"] = self.updated_at.isoformat()
-    new_dict["__class__"] = self.__class__.__name__
-    return new_dict
+    def to_dict(self):
+        """returns a dictionary containing all key/values of __dict__"""
+        new_dict = self.__dict__.copy()
+        new_dict["created_at"] = self.created_at.isoformat()
+        new_dict["updated_at"] = self.updated_at.isoformat()
+        new_dict["__class__"] = self.__class__.__name__
+        return new_dict
