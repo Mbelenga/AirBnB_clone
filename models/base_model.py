d15856c09f429629481d9f52434c5c03cd3cb4ff
Mbelenga <mbelengaterence@gmail.com>
2026-09-11 00:07:55 +0300

    Updated

---
+++ models/base_model.py
@@ -0,0 +1,47 @@
+#!/usr/bin/python3
+""" A model that defines all common attributes """
+import uuid
+from datetime import datetime
+import models
+
+
+class BaseModel:
+    """ public instance attributes """
+
+    def __init__(self, *args, **kwargs):
+        """initialize new instance for BaseModel
+        Args:
+        **kwargs: Dictionary with key and values
+        """
+        f_time = '%Y-%m-%dT%H:%M:%S.%f'
+        if kwargs:
+            for key, value in kwargs.items():
+                if key != "__class__":
+                    if key in ['created_at', 'updated_at']:
+                        setattr(self, key. datetime.strptime(value, f_time))
+                    else:
+                        setattr(self, key, value)
+
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
+
+    def __str__(self):
+        """ Print the class_name id and dict in a specified format """
+        return ("[{}] ({}) {}"
+                .format(self.__class__.__name__, self.id, self.__dict__))
+
+    def save(self):
+        """Update the public instance updated_at with current time."""
+        self.updated_at = datetime.now()
+        models.storage.new(self)
+        models.storage.save()
+
+    def to_dict(self):
+        """Return a dictionary containing all keys/values of __dict__."""
+        obj_dict = self.__dict__.copy()
+        obj_dict["__class__"] = self.__class__.__name__
+        obj_dict["created_at"] = self.created_at.isoformat()
+        obj_dict["updated_at"] = self.updated_at.isoformat()
+        return obj_dict
