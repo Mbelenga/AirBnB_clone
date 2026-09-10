3b504a09fd85bd20d61558cb05f887a332ca2436
Mbelenga <mbelengaterence@gmail.com>
2026-09-10 23:31:10 +0300

    Initializer

--- models/__init__.py
+++ models/__init__.py
@@ -1,6 +1,13 @@
 #!/usr/bin/python3
-""" init models """
 
+"""
+Create a unique FileStorage.
+
+instance for your application
+"""
+
+
+from models.base_model import BaseModel
 from models.engine.file_storage import FileStorage
 
 storage = FileStorage()
