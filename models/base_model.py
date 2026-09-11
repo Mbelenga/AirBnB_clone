46f5a8eaa5ebbf5bb6e0728f8d2ef6ff749660c7
Mbelenga <mbelengaterence@gmail.com>
2026-09-11 20:07:33 +0300

    Updated my base_model.py

--- models/base_model.py
+++ models/base_model.py
@@ -1,16 +1,18 @@
 #!/usr/bin/python3
+
 """Defines the BaseModel"""
 
-
 from uuid import uuid4
-from datetime import date, datetime
+from datetime import datetime
 import models
 
 
 class BaseModel:
     """The super class"""
+
     def __init__(self, *args, **kwargs):
         """Initializing the base model"""
+
         if len(kwargs) != 0:
             for key, value in kwargs.items():
                 if key == "__class__":
@@ -23,22 +25,33 @@ class BaseModel:
             self.id = str(uuid4())
             self.created_at = datetime.now()
             self.updated_at = self.created_at
+
             models.storage.new(self)
 
     def __str__(self):
         """sets the print behaviour of the base model"""
+
         class_name = self.__class__.__name__
-        return "[{}] ({}) {}".format(class_name, self.id, self.__dict__)
+
+        return "[{}] ({}) {}".format(
+            class_name,
+            self.id,
+            self.__dict__
+        )
 
     def save(self):
-        """updates up_dated with current datetime"""
+        """updates updated_at with current datetime"""
+
         self.updated_at = datetime.now()
         models.storage.save()
 
     def to_dict(self):
         """returns a dictionary containing all key/values of __dict__"""
+
         new_dict = self.__dict__.copy()
+
         new_dict["created_at"] = self.created_at.isoformat()
         new_dict["updated_at"] = self.updated_at.isoformat()
         new_dict["__class__"] = self.__class__.__name__
+
         return new_dict
